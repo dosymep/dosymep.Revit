731900322a954439b06433c03f40da95d642fe58
dosymep <dosymep@gmail.com>
2021-06-30 09:34:03 +0300

    Добавил класс расширений для документа Revit

---
+++ dosymep.Revit/DocumentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+namespace dosymep.Revit {
+    public static class DocumentExtensions {
+        public static void UnloadRevitLinks(string rvtFileName) {
+            ModelPath rvtModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(rvtFileName);
+
+            TransmissionData transData = TransmissionData.ReadTransmissionData(rvtModelPath);
+            transData.IsTransmitted = true;
+
+            IEnumerable<ExternalFileReference> externalReferences = transData.GetAllExternalFileReferenceIds()
+                .Select(item => transData.GetLastSavedReferenceData(item))
+                .Where(item => item.ExternalFileReferenceType == ExternalFileReferenceType.RevitLink);
+
+            foreach(ExternalFileReference externalReference in externalReferences) {
+                transData.SetDesiredReferenceData(externalReference.GetReferencingId(), externalReference.GetPath(), externalReference.PathType, false);
+            }
+
+            TransmissionData.WriteTransmissionData(rvtModelPath, transData);
+        }
+    }
+}
