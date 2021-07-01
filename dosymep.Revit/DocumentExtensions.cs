924c84e7a3f3d6e45134325bf875c8bfd7082a72
dosymep <dosymep@gmail.com>
2021-07-01 08:57:38 +0300

    Добавил вспомогательные методы по откреплению связанных файлов

--- dosymep.Revit/DocumentExtensions.cs
+++ dosymep.Revit/DocumentExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,14 @@ using Autodesk.Revit.DB;
 namespace dosymep.Revit {
     public static class DocumentExtensions {
         public static void UnloadRevitLinks(string rvtFileName) {
+            if(string.IsNullOrEmpty(rvtFileName)) {
+                throw new ArgumentException($"'{nameof(rvtFileName)}' cannot be null or empty.", nameof(rvtFileName));
+            }
+
+            if(!File.Exists(rvtFileName)) {
+                throw new FileNotFoundException("Не был найден файл.", rvtFileName);
+            }
+
             ModelPath rvtModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(rvtFileName);
 
             TransmissionData transData = TransmissionData.ReadTransmissionData(rvtModelPath);
@@ -23,6 +32,16 @@ namespace dosymep.Revit {
             }
 
             TransmissionData.WriteTransmissionData(rvtModelPath, transData);
+        }
+
+        public static void UnloadRevitLinks(params string[] rvtFileNames) {
+            UnloadRevitLinks((IEnumerable<string>) rvtFileNames);
+        }
+
+        public static void UnloadRevitLinks(IEnumerable<string> rvtFileNames) {
+            foreach(string rvtFileName in rvtFileNames) {
+                UnloadRevitLinks(rvtFileName);
+            }
         }
     }
 }
