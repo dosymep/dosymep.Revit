d83e26ba3cd9dbcc70adaed45e10a18b97c625f3
dosymep <dosymep@gmail.com>
2021-07-15 15:08:03 +0300

    Добавил классы по работе с TransmissionData

---
+++ dosymep.Revit/Transmissions/ExternalFileReference.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace dosymep.Revit.Transmissions {
+    public enum LoadState {
+        Loaded,
+        Unloaded,
+
+        [XmlEnum(Name = "Not Found")]
+        NotFound
+    }
+
+    public enum PathType {
+        Absolute,
+        Relative,
+
+        [XmlEnum(Name = "Server Location")]
+        ServerLocation,
+
+        [XmlEnum(Name = "Relative to Central Model")]
+        RelativeCentralModel,
+
+        [XmlEnum(Name = "Relative to Library Locations")]
+        RelativeLibraryLocations
+    }
+
+    public enum ExternalFileReferenceType {
+        Decal,
+
+        [XmlEnum(Name = "CAD Link")]
+        CADLink,
+
+        [XmlEnum(Name = "Revit Link")]
+        RevitLink,
+
+        [XmlEnum(Name = "Keynote Table")]
+        KeynoteTable,
+
+        [XmlEnum(Name = "Assembly Code Table")]
+        AssemblyCodeTable,
+
+        [XmlEnum(Name = "DWF Markup")]
+        DWFMarkup
+    }
+
+    [Serializable]
+    public class ExternalFileReference {
+        public int ElementId { get; set; }
+        public ExternalFileReferenceType ExternalFileReferenceType { get; set; }
+
+        public string LastSavedPath { get; set; }
+        public string LastSavedAbsolutePath { get; set; }
+        public string LastSavedCentralServerLocation { get; set; }
+        public PathType LastSavedPathType { get; set; }
+        public LoadState LastSavedLoadState { get; set; }
+
+        public string DesiredPath { get; set; }
+        public string DesiredCentralServerLocation { get; set; }
+        public PathType DesiredPathType { get; set; }
+        public LoadState DesiredLoadState { get; set; }
+    }
+}
---
+++ dosymep.Revit/Transmissions/TransmissionData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace dosymep.Revit.Transmissions {
+    [Serializable]
+    public class TransmissionData {
+
+        [XmlAttribute("isTransmitted")]
+        public bool IsTransmitted { get; set; }
+
+        [XmlAttribute("userData")]
+        public string UserData { get; set; }
+
+        [XmlAttribute("version")]
+        public string Version { get; set; }
+
+        [XmlElement("ExternalFileReference")]
+        public List<ExternalFileReference> ExternalFileReferences { get; set; }
+    }
+}
