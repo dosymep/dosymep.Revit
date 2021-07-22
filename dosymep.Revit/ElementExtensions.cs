9427b6ec939eca72544980209ef6ece5643b91d1
dosymep <dosymep@gmail.com>
2021-07-22 11:00:59 +0300

    Добавил получение параметров через встроенные типы

--- dosymep.Revit/ElementExtensions.cs
+++ dosymep.Revit/ElementExtensions.cs
@@ -8,6 +8,8 @@ using Autodesk.Revit.DB;
 
 namespace dosymep.Revit {
     public static class ElementExtensions {
+        #region Получение параметра по его имени
+
         /// <summary>
         /// Возвращает значение параметра либо значение по умолчанию.
         /// </summary>
@@ -46,24 +48,7 @@ namespace dosymep.Revit {
                 throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
             }
 
-            Parameter param = element.GetParam(paramName);
-            if(param.StorageType == StorageType.Double) {
-                return param.AsDouble();
-            }
-
-            if(param.StorageType == StorageType.Integer) {
-                return param.AsInteger();
-            }
-
-            if(param.StorageType == StorageType.String) {
-                return param.AsString();
-            }
-
-            if(param.StorageType == StorageType.ElementId) {
-                return param.AsElementId();
-            }
-
-            return null;
+            return element.GetParam(paramName).AsObject();
         }
 
         /// <summary>
@@ -138,6 +123,103 @@ namespace dosymep.Revit {
             element.GetParam(paramName).Set(paramValue);
         }
 
+        #endregion
+
+        #region Получение параметра по BuiltInParameter
+
+        /// <summary>
+        /// Возвращает значение параметра либо значение по умолчанию.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <param name="default">Значение по умолчанию.</param>
+        /// <returns>Возвращает значение параметра либо значение по умолчанию.</returns>
+        public static object GetParamValueOrDefault(this Element element, BuiltInParameter builtInParameter, object @default = default) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            try {
+                return element.GetParamValue(builtInParameter);
+            } catch(ArgumentException) {
+                return @default;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает значение параметра элемента.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <returns>Возвращает значение параметра элемента.</returns>
+        public static object GetParamValue(this Element element, BuiltInParameter builtInParameter) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            return element.GetParam(builtInParameter).AsObject();
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetParamValue(this Element element, BuiltInParameter builtInParameter, double paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            element.GetParam(builtInParameter).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetParamValue(this Element element, BuiltInParameter builtInParameter, int paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            element.GetParam(builtInParameter).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetParamValue(this Element element, BuiltInParameter builtInParameter, string paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            element.GetParam(builtInParameter).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetParamValue(this Element element, BuiltInParameter builtInParameter, ElementId paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            element.GetParam(builtInParameter).Set(paramValue);
+        }
+
+        #endregion
+
+        #region Получение параметра
+
         /// <summary>
         /// Возвращает параметр.
         /// </summary>
@@ -149,20 +231,33 @@ namespace dosymep.Revit {
                 throw new ArgumentNullException(nameof(element));
             }
 
-            if(string.IsNullOrEmpty(paramName)) {
-                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
-            }
-
             var param = element.LookupParameter(paramName);
             if(param is null) {
                 throw new ArgumentException($"Общего параметра с заданным именем \"{paramName}\" не существует.", nameof(paramName));
             }
 
-            if(!param.HasValue) {
-                throw new ArgumentException($"Общего параметра с заданным именем \"{paramName}\" не существует.", nameof(paramName));
+            return param;
+        }
+
+        /// <summary>
+        /// Возвращает параметр.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="builtInParameter">Встроенный тип параметра.</param>
+        /// <returns>Возвращает параметр.</returns>
+        public static Parameter GetParam(this Element element, BuiltInParameter builtInParameter) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            var param = element.get_Parameter(builtInParameter);
+            if(param is null) {
+                throw new ArgumentException($"Общего параметра с заданным именем \"{builtInParameter}\" не существует.", nameof(builtInParameter));
             }
 
             return param;
         }
+        
+        #endregion
     }
 }
---
+++ dosymep.Revit/ParamExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+namespace dosymep.Revit {
+    /// <summary>
+    /// Расширения класса параметров.
+    /// </summary>
+    public static class ParamExtensions {
+        /// <summary>
+        /// Возвращает значение параметра.
+        /// </summary>
+        /// <param name="parameter">Параметр.</param>
+        /// <returns>Возвращает значение параметра.</returns>
+        public static object AsObject(this Parameter parameter) {
+            if(parameter.HasValue) {
+                var storageType = parameter.StorageType;
+                if(storageType == StorageType.Integer)
+                    return parameter.AsInteger();
+                else if(storageType == StorageType.Double)
+                    return parameter.AsDouble();
+                else if(storageType == StorageType.String)
+                    return parameter.AsString();
+                else if(storageType == StorageType.ElementId)
+                    return parameter.AsElementId();
+            }
+
+            return null;
+        }
+    }
+}
