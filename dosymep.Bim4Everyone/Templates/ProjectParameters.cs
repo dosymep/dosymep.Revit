06d714b9f1771029d66bca75062ea5bfcf8f80cb
dosymep <dosymep@gmail.com>
2021-08-03 13:30:36 +0300

    Добавил копирование настроек диспетчера видов

---
+++ dosymep.Bim4Everyone/Templates/ProjectParameters.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+
+namespace dosymep.Bim4Everyone.Templates {
+    /// <summary>
+    /// Класс по копирование параметров проекта.
+    /// </summary>
+    public class ProjectParameters {
+        /// <summary>
+        /// Создает экземпляр класса параметров проекта.
+        /// </summary>
+        /// <param name="application">Приложение Revit.</param>
+        /// <returns>Возвращает экземпляр класса параметров проекта.</returns>
+        public static ProjectParameters Create(Application application) {
+            if(application is null) {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            return new ProjectParameters() { Application = application };
+        }
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        internal ProjectParameters() { }
+
+        /// <summary>
+        /// Приложение Revit.
+        /// </summary>
+        public Application Application { get; private set; }
+
+        /// <summary>
+        /// Настраивает диспетчер видов.
+        /// </summary>
+        /// <param name="target">Документ, в котором требуется настроить диспетчер видов.</param>
+        /// <remarks>Метод открывает транзакцию при настройке диспетчера видов.</remarks>
+        public void SetupBrowserOrganization(Document target) {
+            if(Application == null) {
+                throw new InvalidOperationException($"Перед настройкой диспетчера видов нужно инициализировать свойство \"{nameof(Application)}\".");
+            }
+
+            int viewId = 304320;
+            using(var transaction = new Transaction(target)) {
+                transaction.Start($"Настройка диспетчера видов");
+
+                Document source = Application.OpenDocumentFile(ModuleEnvironment.ParametersTemplatePath);
+               
+                CopyView(target, source, viewId);
+                CopyBrowserOrganization(target, source);
+
+                source.Close(false);
+                transaction.Commit();
+            }
+        }
+
+        private static void CopyView(Document target, Document document, int viewId) {
+            ICollection<ElementId> copiedElements = ElementTransformUtils.CopyElements(document, new[] { new ElementId(viewId) }, target, Transform.Identity, new CopyPasteOptions());
+
+            // Удаляем скопированный вид,
+            // так как он нужен был для переноса параметра
+            target.Delete(copiedElements);
+        }
+
+        private static void CopyBrowserOrganization(Document target, Document document) {
+            var elements = new FilteredElementCollector(document).OfClass(typeof(BrowserOrganization)).ToElements();
+
+            // Получаем только те элементы,
+            // у которых совпадает имя с копируемыми настройками диспетчера видов
+            var removingElements = new FilteredElementCollector(target)
+                .OfClass(typeof(BrowserOrganization))
+                .Where(targetItem => elements.Any(item => item.Name.Equals(targetItem.Name)))
+                .Select(item => item.Id)
+                .ToArray();
+
+            // Сначала копируем, после удаляем,
+            // потому что невозможно удалить все настройки диспетчера видов
+            ElementTransformUtils.CopyElements(document, elements.Select(item => item.Id).ToArray(), target, Transform.Identity, new CopyPasteOptions());
+
+            // Удаляем все найденные настройки организации браузера
+            // чтобы произвести замену этих элементов
+            target.Delete(removingElements);
+        }
+    }
+}
