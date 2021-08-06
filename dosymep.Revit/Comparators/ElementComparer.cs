997c15fc8c79ae620b7b9f1f5298d3b03d51098a
dosymep <dosymep@gmail.com>
2021-08-06 14:53:37 +0300

    Добавил документацию для компараторов

--- dosymep.Revit/Comparators/ElementComparer.cs
+++ dosymep.Revit/Comparators/ElementComparer.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 using Autodesk.Revit.DB;
 
 namespace dosymep.Revit.Comparators {
+    /// <summary>
+    /// Компаратор для элемента.
+    /// </summary>
+    /// <remarks>Используется умное сравнение <see cref="LogicalStringComparer"/></remarks>
     public class ElementComparer : IComparer<Element> {
         private readonly LogicalStringComparer _logicalStringComparer = new LogicalStringComparer();
 
+        /// <inheritdoc/>
         public int Compare(Element x, Element y) {
             return _logicalStringComparer.Compare(x?.Name, y?.Name);
         }
--- dosymep.Revit/Comparators/LogicalStringComparer.cs
+++ dosymep.Revit/Comparators/LogicalStringComparer.cs
@@ -6,10 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace dosymep.Revit.Comparators {
+    /// <summary>
+    /// Умное сравнение строк.
+    /// </summary>
+    /// <remarks>Для сравнения используется метод WinApi <see cref="StrCmpLogicalW"/></remarks>
     public class LogicalStringComparer : IComparer<string> {
         [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
         private static extern int StrCmpLogicalW(string x, string y);
 
+        /// <inheritdoc/>
         public int Compare(string x, string y) {
             return StrCmpLogicalW(x, y);
         }
