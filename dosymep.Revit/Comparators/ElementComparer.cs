e0530be95ecc36a31d7896f43a870062c3af381f
dosymep <dosymep@gmail.com>
2021-08-06 14:45:34 +0300

    Добавил компаратор для элемента

---
+++ dosymep.Revit/Comparators/ElementComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+namespace dosymep.Revit.Comparators {
+    public class ElementComparer : IComparer<Element> {
+        private readonly LogicalStringComparer _logicalStringComparer = new LogicalStringComparer();
+
+        public int Compare(Element x, Element y) {
+            return _logicalStringComparer.Compare(x?.Name, y?.Name);
+        }
+    }
+}
---
+++ dosymep.Revit/Comparators/LogicalStringComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosymep.Revit.Comparators {
+    public class LogicalStringComparer : IComparer<string> {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        private static extern int StrCmpLogicalW(string x, string y);
+
+        public int Compare(string x, string y) {
+            return StrCmpLogicalW(x, y);
+        }
+    }
+}
