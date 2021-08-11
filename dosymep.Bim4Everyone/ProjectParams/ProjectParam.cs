ae554afe235fa78247f0682b3f86e351c25b9daf
dosymep <dosymep@gmail.com>
2021-08-11 15:59:26 +0300

    Добавил конфигурацию параметров проекта

---
+++ dosymep.Bim4Everyone/ProjectParams/ProjectParam.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+using pyRevitLabs.Json;
+
+namespace dosymep.Bim4Everyone.ProjectParams {
+    /// <summary>
+    /// Класс параметров проекта Revit.
+    /// </summary>
+    public class ProjectParam : RevitParam {
+        /// <summary>
+        /// Содержит все описания параметров проекта.
+        /// </summary>
+        private readonly static Dictionary<string, string> _description = new Dictionary<string, string>() {
+
+        };
+
+        /// <summary>
+        /// Содержит все типы параметров проекта.
+        /// </summary>
+        private readonly static Dictionary<string, StorageType> _projectParamTypes = new Dictionary<string, StorageType>() {
+
+        };
+
+        /// <summary>
+        /// Конструктор класса параметра проекта.
+        /// </summary>
+        internal ProjectParam() { }
+
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public override string Description {
+            get { return _description.TryGetValue(PropertyName, out string value) ? value : null; }
+        }
+
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public override StorageType SharedParamType {
+            get { return _projectParamTypes.TryGetValue(PropertyName, out StorageType value) ? value : StorageType.None; }
+        }
+    }
+}
---
+++ dosymep.Bim4Everyone/ProjectParams/ProjectParamsConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using pyRevitLabs.Json;
+
+namespace dosymep.Bim4Everyone.ProjectParams {
+    /// <summary>
+    /// Конфигурация параметров проекта.
+    /// </summary>
+    public class ProjectParamsConfig {
+        /// <summary>
+        /// Текущее состояние конфигурации.
+        /// </summary>
+        /// <remarks>Перед использованием нужно вызвать <see cref="Load(string)"/></remarks>
+        public static ProjectParamsConfig Instance { get; internal set; }
+
+        /// <summary>
+        /// Загрузка текущей конфигурации.
+        /// </summary>
+        /// <param name="configPath">Путь до конфигурации.</param>
+        /// <remarks>Возвращает конфигурацию по умолчанию если был найден переданный файл.</remarks>
+        public static void LoadInstance(string configPath) {
+            Instance = Load(configPath);
+        }
+
+        /// <summary>
+        /// Загрузка текущей конфигурации.
+        /// </summary>
+        /// <param name="configPath">Путь до конфигурации.</param>
+        /// <remarks>Возвращает конфигурацию по умолчанию если был найден переданный файл.</remarks>
+        public static ProjectParamsConfig Load(string configPath) {
+            return File.Exists(configPath) ? JsonConvert.DeserializeObject<ProjectParamsConfig>(File.ReadAllText(configPath)) : GetDefaultConfg();
+        }
+
+        /// <summary>
+        /// Возвращает конфигурацию по умолчанию.
+        /// </summary>
+        /// <returns>Возвращает конфигурацию по умолчанию.</returns>
+        public static ProjectParamsConfig GetDefaultConfg() {
+            return new ProjectParamsConfig();
+        }
+    }
+}
