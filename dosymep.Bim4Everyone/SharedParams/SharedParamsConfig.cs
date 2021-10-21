729aa48365383105dca7e5cfb763b60b68c7d51b
dosymep <dosymep@gmail.com>
2021-10-21 15:57:52 +0300

    Добавил общие параметры квартирографии

--- dosymep.Bim4Everyone/SharedParams/SharedParamsConfig.cs
+++ dosymep.Bim4Everyone/SharedParams/SharedParamsConfig.cs
@@ -82,6 +82,142 @@ namespace dosymep.Bim4Everyone.SharedParams {
 
         #endregion
 
+        #region Квартирография
+
+        /// <summary>
+        /// ФОП_ПМЩ_Площадь
+        /// </summary>
+        public SharedParam RoomArea { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomArea), Name = "ФОП_ПМЩ_Площадь" };
+
+        /// <summary>
+        /// ФОП_ПМЩ_Площадь с коэф.
+        /// </summary>
+        public SharedParam RoomAreaWithRatio { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomAreaWithRatio), Name = "ФОП_ПМЩ_Площадь с коэф." };
+
+        /// <summary>
+        /// ФОП_Коэффициент площади
+        /// </summary>
+        public SharedParam RoomAreaRatio { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomAreaRatio), Name = "ФОП_Коэффициент площади" };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь с коэф.
+        /// </summary>
+        public SharedParam ApartmentAreaRatio { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaRatio), Name = "ФОП_КВР_Площадь с коэф." };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь без ЛП
+        /// </summary>
+        public SharedParam ApartmentAreaNoBalcony { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaNoBalcony), Name = "ФОП_КВР_Площадь без ЛП" };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь жилая
+        /// </summary>
+        public SharedParam ApartmentLivingArea { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentLivingArea), Name = "ФОП_КВР_Площадь жилая" };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь без коэф.
+        /// </summary>
+        public SharedParam ApartmentArea { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentArea), Name = "ФОП_КВР_Площадь без коэф." };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь по пятну
+        /// </summary>
+        public SharedParam ApartmentFullArea { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentFullArea), Name = "ФОП_КВР_Площадь по пятну" };
+
+        /// <summary>
+        /// ФОП_ПМЩ_Группа
+        /// </summary>
+        public SharedParam RoomGroupName { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomGroupName), Name = "ФОП_ПМЩ_Группа" };
+
+        /// <summary>
+        /// ФОП_Доп. сортировка групп
+        /// </summary>
+        public SharedParam ApartmentGroupName { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentGroupName), Name = "ФОП_Доп. сортировка групп" };
+
+        /// <summary>
+        /// ФОП_Пожарный отсек
+        /// </summary>
+        public SharedParam FireCompartmentName { get; internal set; } = new SharedParam() { PropertyName = nameof(FireCompartmentName), Name = "ФОП_Пожарный отсек" };
+
+        /// <summary>
+        /// ФОП_ПМЩ_Секция
+        /// </summary>
+        public SharedParam RoomSectionName { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomSectionName), Name = "ФОП_ПМЩ_Секция" };
+
+        /// <summary>
+        /// ФОП_Тип группы помещений
+        /// </summary>
+        public SharedParam RoomTypeGroupName { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomTypeGroupName), Name = "ФОП_Тип группы помещений" };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь по ТЗ
+        /// </summary>
+        public SharedParam ApartmentAreaSpec { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaSpec), Name = "ФОП_КВР_Площадь по ТЗ" };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь по ТЗ мин
+        /// </summary>
+        public SharedParam ApartmentAreaMinSpec { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaMinSpec), Name = "ФОП_КВР_Площадь по ТЗ мин" };
+
+        /// <summary>
+        /// ФОП_КВР_Площадь по ТЗ макс
+        /// </summary>
+        public SharedParam ApartmentAreaMaxSpec { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaMaxSpec), Name = "ФОП_КВР_Площадь по ТЗ макс" };
+
+        /// <summary>
+        /// ФОП_Номер квартиры
+        /// </summary>
+        public SharedParam ApartmentNumber { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentNumber), Name = "ФОП_Номер квартиры" };
+
+        /// <summary>
+        /// ФОП_Доп. номер помещения
+        /// </summary>
+        public SharedParam ApartmentNumberExtra { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentNumberExtra), Name = "ФОП_Доп. номер помещения" };
+
+        /// <summary>
+        /// ФОП_ФИКС_КВР_Площадь с коэф.
+        /// </summary>
+        public SharedParam ApartmentAreaRatioFix { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaRatioFix), Name = "ФОП_ФИКС_КВР_Площадь с коэф." };
+
+        /// <summary>
+        /// ФОП_ФИКС_КВР_Площадь без ЛП
+        /// </summary>
+        public SharedParam ApartmentAreaNoBalconyFix { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaNoBalconyFix), Name = "ФОП_ФИКС_КВР_Площадь без ЛП" };
+
+        /// <summary>
+        /// ФОП_ФИКС_КВР_Площадь жилая
+        /// </summary>
+        public SharedParam ApartmentLivingAreaFix { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentLivingAreaFix), Name = "ФОП_ФИКС_КВР_Площадь жилая" };
+
+        /// <summary>
+        /// ФОП_ФИКС_КВР_Площадь без коэф.
+        /// </summary>
+        public SharedParam ApartmentAreaFix { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentAreaFix), Name = "ФОП_ФИКС_КВР_Площадь без коэф." };
+
+        /// <summary>
+        /// ФОП_ФИКС_ПМЩ_Площадь
+        /// </summary>
+        public SharedParam RoomAreaFix { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomAreaFix), Name = "ФОП_ФИКС_ПМЩ_Площадь" };
+
+        /// <summary>
+        /// ФОП_ФИКС_ПМЩ_Площадь с коэф.
+        /// </summary>
+        public SharedParam RoomAreaWithRatioFix { get; internal set; } = new SharedParam() { PropertyName = nameof(RoomAreaWithRatioFix), Name = "ФОП_ФИКС_ПМЩ_Площадь с коэф." };
+
+        /// <summary>
+        /// ФОП_ФИКС_КВР_Площадь по пятну
+        /// </summary>
+        public SharedParam ApartmentFullAreaFix { get; internal set; } = new SharedParam() { PropertyName = nameof(ApartmentFullAreaFix), Name = "ФОП_ФИКС_КВР_Площадь по пятну" };
+
+        #endregion
+
+        /// <summary>
+        /// ФОП_Этаж
+        /// </summary>
+        public SharedParam Level { get; internal set; } = new SharedParam() { PropertyName = nameof(Level), Name = "ФОП_Этаж" };
+
+
+
         /// <summary>
         /// Загрузка текущей конфигурации.
         /// </summary>
