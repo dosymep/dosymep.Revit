cbf1b7ec3cbf5f28654830131e427246f3d14afd
dosymep <dosymep@gmail.com>
2021-10-25 09:12:16 +0300

    Добавил конфигурацию системных атрибутов

---
+++ dosymep.Bim4Everyone/SystemParams/SystemParam.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+
+namespace dosymep.Bim4Everyone.SystemParams {
+    /// <summary>
+    /// Класс системного параметра.
+    /// </summary>
+    public class SystemParam : RevitParam {
+        private readonly LanguageType? _languageType;
+
+        /// <summary>
+        /// Создает экземпляр класса системного параметра.
+        /// </summary>
+        /// <param name="languageType">Язык системы.</param>
+        /// <param name="builtInParameter">Системный параметр.</param>
+        internal SystemParam(LanguageType? languageType, BuiltInParameter builtInParameter) {
+            _languageType = languageType;
+            BuiltInParameter = builtInParameter;
+        }
+
+        /// <inheritdoc/>
+        public override string Name {
+            get {
+                if(_languageType.HasValue) {
+                    return LabelUtils.GetLabelFor(BuiltInParameter, _languageType.Value);
+                }
+                
+                return LabelUtils.GetLabelFor(BuiltInParameter);
+            }
+            
+            set => throw new NotSupportedException($"Для установки имени параметра нужно использовать свойство \"{nameof(BuiltInParameter)}\".");
+        }
+
+        /// <summary>
+        /// Системное наименование параметра.
+        /// </summary>
+        public BuiltInParameter BuiltInParameter { get; }
+    }
+}
---
+++ dosymep.Bim4Everyone/SystemParams/SystemParamsConfig.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+
+namespace dosymep.Bim4Everyone.SystemParams {
+    /// <summary>
+    /// Конфигурация системных параметров.
+    /// </summary>
+    public class SystemParamsConfig : RevitParamsConfig {
+        private readonly LanguageType? _languageType;
+
+        /// <summary>
+        /// Инициализирует конфигурацию системных параметров.
+        /// </summary>
+        internal SystemParamsConfig() { }
+
+        /// <summary>
+        /// Инициализирует конфигурацию системных параметров.
+        /// </summary>
+        /// <param name="languageType">Язык системы.</param>
+        internal SystemParamsConfig(LanguageType? languageType) {
+            _languageType = languageType;
+        }
+
+        /// <summary>
+        /// Текущее состояние конфигурации.
+        /// </summary>
+        /// <remarks>Перед использованием нужно вызвать <see cref="LoadInstance(Autodesk.Revit.ApplicationServices.LanguageType)"/></remarks>
+        public static SystemParamsConfig Instance { get; internal set; }
+
+        /// <summary>
+        /// Возвращает экземпляр класса системного параметра.
+        /// </summary>
+        /// <param name="builtInParameter">Системный параметр.</param>
+        /// <returns>Возвращает экземпляр класса системного параметра.</returns>
+        public SystemParam GetSystemParam(BuiltInParameter builtInParameter) {
+            return new SystemParam(_languageType, builtInParameter: builtInParameter);
+        }
+
+        /// <inheritdoc/>
+        public override void Save(string configPath) {
+            throw new NotSupportedException("Сохранение конфигурации для системных параметров бесполезно.");
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<RevitParam> GetSharedParams() {
+            return Enum.GetValues(typeof(BuiltInParameter)).Cast<BuiltInParameter>().Select(item => GetSystemParam(item));
+        }
+
+        /// <summary>
+        /// Загрузка текущей конфигурации.
+        /// </summary>
+        /// <param name="languageType">Язык системы.</param>
+        public static void LoadInstance(LanguageType? languageType) {
+            Instance = languageType.HasValue ? Load(languageType) : GetDefaultConfg();
+        }
+
+        /// <summary>
+        /// Загрузка текущей конфигурации.
+        /// </summary>
+        /// <param name="languageType">Язык системы.</param>
+        public static SystemParamsConfig Load(LanguageType? languageType) {
+            return new SystemParamsConfig(languageType);
+        }
+
+        /// <summary>
+        /// Возвращает конфигурацию по умолчанию.
+        /// </summary>
+        /// <returns>Возвращает конфигурацию по умолчанию.</returns>
+        public static SystemParamsConfig GetDefaultConfg() {
+            return new SystemParamsConfig();
+        }
+    }
+}
