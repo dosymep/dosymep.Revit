a5be195e356720b4b7447c2b2629998c24948fe4
dosymep <dosymep@gmail.com>
2021-11-16 11:44:31 +0300

    Добавил класс расширений транзакций, добавил метод добавляющий текст BIM

---
+++ dosymep.Revit/TransactionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+namespace dosymep.Revit {
+    /// <summary>
+    /// Расширения класса <see cref="Autodesk.Revit.DB.Transaction"/>
+    /// </summary>
+    public static class TransactionExtensions {
+        /// <summary>
+        /// Добавляет в название транзакции текст BIM.
+        /// </summary>
+        /// <param name="transaction">Транзакция.</param>
+        /// <param name="transactionName">Название транзакции.</param>
+        /// <returns>Возвращает статус запуска транзакции.</returns>
+        public static TransactionStatus BIMStart(this Transaction transaction, string transactionName) {
+            return transaction.Start("BIM: " + transactionName);
+        }
+    }
+}
