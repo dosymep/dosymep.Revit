bc540b00614de9199e1fe6d1f0085354c4aa6845
dosymep <dosymep@gmail.com>
2022-03-04 11:50:19 +0300

    Добавил библиотеки сервисов

---
+++ dosymep.Bim4Everyone/SimpleServices/ServicesProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace dosymep.Bim4Everyone.SimpleServices {
+    /// <summary>
+    /// Класс предоставляющий доступ к внутренним сервисам платформы
+    /// </summary>
+    public static class ServicesProvider {
+        
+    }
+}
