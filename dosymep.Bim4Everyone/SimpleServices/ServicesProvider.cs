2be74255d8ce71ebb892101c39b0c0054814d8c5
dosymep <dosymep@gmail.com>
2022-03-04 11:53:30 +0300

    Добавил инициализацию сервисов платформы

--- dosymep.Bim4Everyone/SimpleServices/ServicesProvider.cs
+++ dosymep.Bim4Everyone/SimpleServices/ServicesProvider.cs
@@ -1,8 +1,29 @@
-﻿namespace dosymep.Bim4Everyone.SimpleServices {
+﻿using Autofac;
+
+using dosymep.SimpleServices;
+using dosymep.Xpf.Core.SimpleServices;
+
+namespace dosymep.Bim4Everyone.SimpleServices {
     /// <summary>
     /// Класс предоставляющий доступ к внутренним сервисам платформы
     /// </summary>
     public static class ServicesProvider {
-        
+        /// <summary>
+        /// Контейнер сервисов платформы.
+        /// </summary>
+        public static IContainer PlatformContainer { get; }
+
+        static ServicesProvider() {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<XtraDispatcherService>().As<IDispatcherService>().SingleInstance();
+            builder.RegisterType<XtraMessageBoxService>().As<IMessageBoxService>().SingleInstance();
+            builder.RegisterType<XtraNotificationService>().As<INotificationService>().SingleInstance();
+            builder.RegisterType<XtraOpenFileDialogService>().As<IOpenFileDialogService>().SingleInstance();
+            builder.RegisterType<XtraSaveFileDialogService>().As<ISaveFileDialogService>().SingleInstance();
+            builder.RegisterType<XtraOpenFolderDialogService>().As<IOpenFolderDialogService>().SingleInstance();
+
+            PlatformContainer = builder.Build();
+        }
     }
 }
