ac89629cd66fb6166b176dcbc97f24d1ae8e7eeb
dosymep <dosymep@gmail.com>
2022-03-05 11:05:00 +0300

    Добавил инициализацию логгера

---
+++ dosymep.Bim4Everyone/SimpleServices/SerilogService.cs
@@ -0,0 +1,325 @@
+﻿using System;
+
+using dosymep.SimpleServices;
+
+using Serilog;
+using Serilog.Core;
+
+namespace dosymep.Bim4Everyone.SimpleServices
+{
+    internal class SerilogService : ILoggerService
+    {
+        private readonly ILogger _logger;
+
+        public SerilogService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Verbose(string messageTemplate)
+        {
+            _logger.Verbose(messageTemplate);
+        }
+
+        public void Verbose<T>(string messageTemplate, T propertyValue)
+        {
+            _logger.Verbose(messageTemplate, propertyValue);
+        }
+
+        public void Verbose<T0, T1>(string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Verbose(messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Verbose<T0, T1, T2>(string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+            _logger.Verbose(messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Verbose(Exception exception, string messageTemplate)
+        {
+            _logger.Verbose(exception, messageTemplate);
+        }
+
+        public void Verbose<T>(Exception exception, string messageTemplate, T propertyValue)
+        {
+            _logger.Verbose(exception, messageTemplate, propertyValue);
+        }
+
+        public void Verbose<T0, T1>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Verbose(exception, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Verbose<T0, T1, T2>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1,
+            T2 propertyValue2)
+        {
+            _logger.Verbose(exception, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Verbose(string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Verbose(messageTemplate, propertyValues);
+        }
+
+        public void Verbose(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Verbose(exception, messageTemplate, propertyValues);
+        }
+        
+        public void Information(string messageTemplate)
+        {
+            _logger.Information(messageTemplate);
+        }
+
+        public void Information<T>(string messageTemplate, T propertyValue)
+        {
+            _logger.Information(messageTemplate, propertyValue);
+        }
+
+        public void Information<T0, T1>(string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Information(messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Information<T0, T1, T2>(string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+            _logger.Information(messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Information(Exception exception, string messageTemplate)
+        {
+            _logger.Information(exception, messageTemplate);
+        }
+
+        public void Information<T>(Exception exception, string messageTemplate, T propertyValue)
+        {
+            _logger.Information(exception, messageTemplate, propertyValue);
+        }
+
+        public void Information<T0, T1>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Information(exception, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Information<T0, T1, T2>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1,
+            T2 propertyValue2)
+        {
+            _logger.Information(exception, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Information(string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Information(messageTemplate, propertyValues);
+        }
+
+        public void Information(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Information(exception, messageTemplate, propertyValues);
+        }
+
+        public void Debug(string messageTemplate)
+        {
+            _logger.Debug(messageTemplate);
+        }
+
+        public void Debug<T>(string messageTemplate, T propertyValue)
+        {
+            _logger.Debug(messageTemplate, propertyValue);
+        }
+
+        public void Debug<T0, T1>(string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Debug(messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Debug<T0, T1, T2>(string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+            _logger.Debug(messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Debug(Exception exception, string messageTemplate)
+        {
+            _logger.Debug(exception, messageTemplate);
+        }
+
+        public void Debug<T>(Exception exception, string messageTemplate, T propertyValue)
+        {
+            _logger.Debug(exception, messageTemplate, propertyValue);
+        }
+
+        public void Debug<T0, T1>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Debug(exception, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Debug<T0, T1, T2>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1,
+            T2 propertyValue2)
+        {
+            _logger.Debug(exception, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Debug(string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Debug(messageTemplate, propertyValues);
+        }
+
+        public void Debug(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Debug(exception, messageTemplate, propertyValues);
+        }
+        
+        public void Warning(string messageTemplate)
+        {
+            _logger.Warning(messageTemplate);
+        }
+
+        public void Warning<T>(string messageTemplate, T propertyValue)
+        {
+            _logger.Warning(messageTemplate, propertyValue);
+        }
+
+        public void Warning<T0, T1>(string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Warning(messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Warning<T0, T1, T2>(string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+            _logger.Warning(messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Warning(Exception exception, string messageTemplate)
+        {
+            _logger.Warning(exception, messageTemplate);
+        }
+
+        public void Warning<T>(Exception exception, string messageTemplate, T propertyValue)
+        {
+            _logger.Warning(exception, messageTemplate, propertyValue);
+        }
+
+        public void Warning<T0, T1>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Warning(exception, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Warning<T0, T1, T2>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1,
+            T2 propertyValue2)
+        {
+            _logger.Warning(exception, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Warning(string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Warning(messageTemplate, propertyValues);
+        }
+
+        public void Warning(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Warning(exception, messageTemplate, propertyValues);
+        }
+
+        public void Error(string messageTemplate)
+        {
+            _logger.Error(messageTemplate);
+        }
+
+        public void Error<T>(string messageTemplate, T propertyValue)
+        {
+            _logger.Error(messageTemplate, propertyValue);
+        }
+
+        public void Error<T0, T1>(string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Error(messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Error<T0, T1, T2>(string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+            _logger.Error(messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Error(Exception exception, string messageTemplate)
+        {
+            _logger.Error(exception, messageTemplate);
+        }
+
+        public void Error<T>(Exception exception, string messageTemplate, T propertyValue)
+        {
+            _logger.Error(exception, messageTemplate, propertyValue);
+        }
+
+        public void Error<T0, T1>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Error(exception, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Error<T0, T1, T2>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1,
+            T2 propertyValue2)
+        {
+            _logger.Error(exception, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Error(string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Error(messageTemplate, propertyValues);
+        }
+
+        public void Error(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Error(exception, messageTemplate, propertyValues);
+        }
+        
+        public void Fatal(string messageTemplate)
+        {
+            _logger.Fatal(messageTemplate);
+        }
+
+        public void Fatal<T>(string messageTemplate, T propertyValue)
+        {
+            _logger.Fatal(messageTemplate, propertyValue);
+        }
+
+        public void Fatal<T0, T1>(string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Fatal(messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Fatal<T0, T1, T2>(string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+            _logger.Fatal(messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Fatal(Exception exception, string messageTemplate)
+        {
+            _logger.Fatal(exception, messageTemplate);
+        }
+
+        public void Fatal<T>(Exception exception, string messageTemplate, T propertyValue)
+        {
+            _logger.Fatal(exception, messageTemplate, propertyValue);
+        }
+
+        public void Fatal<T0, T1>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+            _logger.Fatal(exception, messageTemplate, propertyValue0, propertyValue1);
+        }
+
+        public void Fatal<T0, T1, T2>(Exception exception, string messageTemplate, T0 propertyValue0, T1 propertyValue1,
+            T2 propertyValue2)
+        {
+            _logger.Fatal(exception, messageTemplate, propertyValue0, propertyValue1, propertyValue2);
+        }
+
+        public void Fatal(string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Fatal(messageTemplate, propertyValues);
+        }
+
+        public void Fatal(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            _logger.Fatal(exception, messageTemplate, propertyValues);
+        }
+    }
+}
--- dosymep.Bim4Everyone/SimpleServices/ServicesProvider.cs
+++ dosymep.Bim4Everyone/SimpleServices/ServicesProvider.cs
@@ -1,7 +1,18 @@
-﻿using Autofac;
+﻿using System;
+using System.IO;
+using System.Windows;
+
+using Autodesk.Revit.UI;
+
+using Ninject;
 
 using dosymep.SimpleServices;
 using dosymep.Xpf.Core.SimpleServices;
+
+using Ninject.Activation;
+
+using Serilog;
+using Serilog.Events;
 
 namespace dosymep.Bim4Everyone.SimpleServices {
     /// <summary>
@@ -11,22 +22,45 @@ namespace dosymep.Bim4Everyone.SimpleServices {
         /// <summary>
         /// Контейнер сервисов платформы.
         /// </summary>
-        public static IContainer Instance { get; private set; }
+        public static IKernel Instance { get; private set; }
 
         /// <summary>
         /// Загружает сервисы платформы.
         /// </summary>
-        public static void LoadInstance() {
-            var builder = new ContainerBuilder();
+        public static void LoadInstance(UIApplication uiApplication) {
+            Instance?.Dispose();
+            Instance = new StandardKernel();
 
-            builder.RegisterType<XtraDispatcherService>().As<IDispatcherService>().SingleInstance();
-            builder.RegisterType<XtraMessageBoxService>().As<IMessageBoxService>().SingleInstance();
-            builder.RegisterType<XtraNotificationService>().As<INotificationService>().SingleInstance();
-            builder.RegisterType<XtraOpenFileDialogService>().As<IOpenFileDialogService>().SingleInstance();
-            builder.RegisterType<XtraSaveFileDialogService>().As<ISaveFileDialogService>().SingleInstance();
-            builder.RegisterType<XtraOpenFolderDialogService>().As<IOpenFolderDialogService>().SingleInstance();
+            Instance.Bind<IDispatcherService>().To<XtraDispatcherService>().InSingletonScope();
+            Instance.Bind<IMessageBoxService>().To<XtraMessageBoxService>().InSingletonScope();
+            Instance.Bind<INotificationService>().To<XtraNotificationService>().InSingletonScope();
+            Instance.Bind<IOpenFileDialogService>().To<XtraOpenFileDialogService>().InSingletonScope();
+            Instance.Bind<ISaveFileDialogService>().To<XtraSaveFileDialogService>().InSingletonScope();
+            Instance.Bind<IOpenFolderDialogService>().To<XtraOpenFolderDialogService>().InSingletonScope();
 
-            Instance = builder.Build();
+
+            Instance.Bind<ILoggerService>().To<SerilogService>().InSingletonScope();
+            Instance.Bind<ILogger>().ToMethod(context => InitLogger(context, uiApplication)).InSingletonScope();
+        }
+
+        private static ILogger InitLogger(IContext context, UIApplication uiApplication) {
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "pyRevit", "Logs", uiApplication.Application.VersionNumber, "platform_.log");
+
+            var loggerConfiguration = new LoggerConfiguration()
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .Enrich.WithProperty("EnvironmentUserName", Environment.UserName)
+                .Enrich.WithProperty("RevitVersion", uiApplication.Application.VersionBuild)
+                .Enrich.WithProperty("AutodeskUsername", uiApplication.Application.Username)
+                .Enrich.WithProperty("AutodeskLoginUserId", uiApplication.Application.LoginUserId)
+                .WriteTo.File(fileName, rollingInterval: RollingInterval.Day, 
+                    outputTemplate:"[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] "
+                                   //+ "{{\"MachineName\": \"{MachineName}\", \"UserName\": \"{EnvironmentUserName}\", \"$type\": \"Windows\"}} "
+                                   + "{{\"RevitVersion\": \"{RevitVersion}\", \"UserName\": \"{AutodeskUsername}\", \"LoginUserId\": \"{AutodeskLoginUserId}\", \"$type\": \"Autodesk\"}} "
+                                   + "{Message}{NewLine}{Exception}")
+                .MinimumLevel.Verbose();
+
+            return loggerConfiguration.CreateLogger();
         }
 
         /// <summary>
@@ -35,7 +69,7 @@ namespace dosymep.Bim4Everyone.SimpleServices {
         /// <typeparam name="T">Запрашиваемый сервис.</typeparam>
         /// <returns>Возвращает сервис из контейнера.</returns>
         public static T GetPlatformService<T>() {
-            return Instance.Resolve<T>();
+            return Instance.Get<T>();
         }
     }
 }
