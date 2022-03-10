d8de78624a06e10edd3f2975f2c2765c68fbbf84
dosymep <dosymep@gmail.com>
2022-03-10 10:11:34 +0300

    Добавил сервис темы Revit

---
+++ dosymep.Bim4Everyone/SimpleServices/RevitThemeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Autodesk.Revit.UI;
+
+using dosymep.SimpleServices;
+
+namespace dosymep.Bim4Everyone.SimpleServices {
+    internal class RevitThemeService : IUIThemeService {
+        public event Action<UIThemes> UIThemeChanged;
+        public UIThemes HostTheme => GetRevitUITheme(); 
+
+        private UIThemes GetRevitUITheme() {
+            if(UIThemeManager.CurrentTheme == UITheme.Dark) {
+                return UIThemes.Dark;
+            } else if(UIThemeManager.CurrentTheme == UITheme.Light) {
+                return UIThemes.Light;
+            }
+
+            return UIThemes.Light;
+        }
+    }
+}
