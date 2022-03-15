9158a508f0fc5bccb4751c90228ff163e4e0f806
dosymep <dosymep@gmail.com>
2022-03-15 10:00:44 +0300

    Добавил расширения для элементов параметров

---
+++ dosymep.Revit/ParamElementExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.DB;
+
+namespace dosymep.Revit {
+    /// <summary>
+    /// Класс расширений элементов параметров
+    /// </summary>
+    public static class ParamElementExtensions {
+        /// <summary>
+        /// Проверяет, является ли параметр параметром проекта.
+        /// </summary>
+        /// <param name="parameterElement">Элемент параметра.</param>
+        /// <returns>Возвращает true - если параметр является параметром проекта, иначе false.</returns>
+        public static bool IsProjectParam(this ParameterElement parameterElement) {
+            return !(parameterElement.IsSharedParam() || parameterElement.IsGlobalParam());
+        }
+        
+        /// <summary>
+        /// Проверяет, является ли параметр общим параметром.
+        /// </summary>
+        /// <param name="parameterElement">Элемент параметра.</param>
+        /// <returns>Возвращает true - если параметр является общим параметром, иначе false.</returns>
+        public static bool IsSharedParam(this ParameterElement parameterElement) {
+            return parameterElement is SharedParameterElement;
+        }
+        
+        /// <summary>
+        /// Проверяет, является ли параметр глобальным параметром.
+        /// </summary>
+        /// <param name="parameterElement">Элемент параметра.</param>
+        /// <returns>Возвращает true - если параметр является глобальным параметром, иначе false.</returns>
+        public static bool IsGlobalParam(this ParameterElement parameterElement) {
+            return parameterElement is GlobalParameter;
+        }
+    }
+}
