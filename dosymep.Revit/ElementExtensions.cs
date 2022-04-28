c5ef250f6f05dcd5662139a9ecd3a74a12425b3d
dosymep <dosymep@gmail.com>
2022-04-28 16:22:41 +0300

    Добавил методы получения значения параметра проекта

--- dosymep.Revit/ElementExtensions.cs
+++ dosymep.Revit/ElementExtensions.cs
@@ -419,6 +419,210 @@ namespace dosymep.Revit {
 
         #endregion
         
+        #region Получение параметра проекта по его имени
+
+        /// <summary>
+        /// Проверяет на существование параметра в элементе.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <returns>Возвращает true - если параметр существует, иначе false.</returns>
+        public static bool IsExistsProjectParam(this Element element, string paramName) {
+            return element.GetProjectParamValueOrDefault(paramName) != default;
+        }
+
+        /// <summary>
+        /// Возвращает значение параметра либо значение по умолчанию.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <param name="default">Значение по умолчанию.</param>
+        /// <returns>Возвращает значение параметра либо значение по умолчанию.</returns>
+        public static T GetProjectParamValueOrDefault<T>(this Element element, string paramName, T @default = default) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            try {
+                object value = element.GetProjectParamValue(paramName);
+                return value == null ? @default : (T) value;
+            } catch(ArgumentException) {
+                return @default;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает значение параметра либо значение по умолчанию.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <param name="default">Значение по умолчанию.</param>
+        /// <returns>Возвращает значение параметра либо значение по умолчанию.</returns>
+        public static object GetProjectParamValueOrDefault(this Element element, string paramName, object @default = default) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            try {
+                return element.GetProjectParamValue(paramName) ?? @default;
+            } catch(ArgumentException) {
+                return @default;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает значение параметра элемента.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <returns>Возвращает значение параметра элемента.</returns>
+        public static T GetProjectParamValue<T>(this Element element, string paramName) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            object value = element.GetProjectParam(paramName).AsObject();
+            return value == null ? default : (T) value;
+        }
+
+        /// <summary>
+        /// Возвращает значение параметра элемента.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <returns>Возвращает значение параметра элемента.</returns>
+        public static object GetProjectParamValue(this Element element, string paramName) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            return element.GetProjectParam(paramName).AsObject();
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetProjectParamValue(this Element element, string paramName, double paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            element.GetProjectParam(paramName).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetProjectParamValue(this Element element, string paramName, int paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            element.GetProjectParam(paramName).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetProjectParamValue(this Element element, string paramName, string paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            element.GetProjectParam(paramName).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Устанавливает значение параметра.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <param name="paramValue">Значение общего параметра.</param>
+        public static void SetProjectParamValue(this Element element, string paramName, ElementId paramValue) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if(string.IsNullOrEmpty(paramName)) {
+                throw new ArgumentException($"'{nameof(paramName)}' cannot be null or empty.", nameof(paramName));
+            }
+
+            element.GetProjectParam(paramName).Set(paramValue);
+        }
+
+        /// <summary>
+        /// Удаляет параметр по его имени.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <returns>Возвращает признак удаления параметра true - если был удален, иначе false.</returns>
+        public static bool RemoveProjectParamValue(this Element element, string paramName) {
+            try {
+                element.GetProjectParam(paramName).RemoveValue();
+                return true;
+            } catch {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает параметр.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        /// <param name="paramName">Наименование параметра.</param>
+        /// <returns>Возвращает параметр.</returns>
+        public static Parameter GetProjectParam(this Element element, string paramName) {
+            if(element is null) {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            Parameter param = element.GetParameters(paramName).FirstOrDefault(item => !item.IsShared);
+            if(param is null) {
+                throw new ArgumentException($"Параметра с заданным именем \"{paramName}\" у элемента не существует.", nameof(paramName));
+            }
+
+            return param;
+        }
+
+        #endregion
+        
         #region Получение параметра по BuiltInParameter
 
         /// <summary>
