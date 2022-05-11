9bad641ea44c1e8d546f6a8d750ee27f6ca4f4b8
dosymep <dosymep@gmail.com>
2022-05-11 10:51:23 +0300

    Исправил комментарий

--- dosymep.Revit/ElementIdExtensions.cs
+++ dosymep.Revit/ElementIdExtensions.cs
@@ -18,7 +18,7 @@ namespace dosymep.Revit {
         /// Проверяет идентификатор, является ли он системным.
         /// </summary>
         /// <param name="elementId">Идентификатор элемента.</param>
-        /// <returns>Возвращает <see cref="bool.True"/> - если идентификатор элемента является системным, иначе false.</returns>
+        /// <returns>Возвращает true - если идентификатор элемента является системным, иначе false.</returns>
         public static bool IsSystemId(this ElementId elementId) {
             return !elementId.IsNull() && elementId.IntegerValue < 0;
         }
