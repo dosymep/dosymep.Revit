22900af91d4a565bc0a8fd48dd3a0a73ddce2ccd
dosymep <dosymep@gmail.com>
2022-05-19 14:51:16 +0300

    Добавил прототип интерфейса создания параметров

---
+++ dosymep.Bim4Everyone/FamilyManagerExtensions.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+using Autodesk.Revit.DB;
+
+namespace dosymep.Bim4Everyone {
+    /// <summary>
+    /// Класс расширений для менеджера семейств.
+    /// </summary>
+    public static class FamilyManagerExtensions {
+        /// <summary>
+        /// Создает параметр типа у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <returns>Возвращает новый созданный параметр типа у семейства.</returns>
+        public static FamilyParameter CreateTypeParam(this FamilyManager self,
+            RevitParam revitParam,
+            BuiltInParameterGroup group) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр типа семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="category">Категория, к которой привязывается новый параметр семейства.</param>
+        /// <returns>Возвращает новый созданный параметр типа у семейства.</returns>
+        public static FamilyParameter CreateTypeParam(this FamilyManager self,
+            RevitParam revitParam,
+            BuiltInParameterGroup group,
+            Category category) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр экземпляра у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <returns>Возвращает новый созданный параметр экземпляра у семейства.</returns>
+        public static FamilyParameter CreateInstanceParam(this FamilyManager self,
+            RevitParam revitParam,
+            BuiltInParameterGroup group) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр экземпляра семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="category">Категория, к которой привязывается новый параметр семейства.</param>
+        /// <returns>Возвращает новый созданный параметр экземпляра у семейства.</returns>
+        public static FamilyParameter CreateInstanceParam(this FamilyManager self,
+            RevitParam revitParam,
+            BuiltInParameterGroup group,
+            Category category) {
+            throw new NotImplementedException();
+        }
+
+#if D2020 || R2020 || R2021 || R2021
+
+        /// <summary>
+        /// Создает параметр типа у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="parameterType">Тип создаваемого параметра у семейства.</param>
+        /// <returns>Возвращает новый созданный параметр типа у семейства.</returns>
+        public static FamilyParameter CreateTypeParam(this FamilyManager self,
+            RevitParam revitParam,
+            BuiltInParameterGroup group,
+            ParameterType parameterType) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр экземпляра у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="parameterType">Тип создаваемого параметра у семейства.</param>
+        /// <returns>Возвращает новый созданный параметр экземпляра у семейства.</returns>
+        public static FamilyParameter CreateInstanceParam(this FamilyManager self,
+            RevitParam revitParam,
+            BuiltInParameterGroup group,
+            ParameterType parameterType) {
+            throw new NotImplementedException();
+        }
+
+#else
+
+        /// <summary>
+        /// Создает параметр типа у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="category">Категория, к которой привязывается новый параметр семейства.</param>
+        /// <returns>Возвращает новый созданный параметр типа у семейства.</returns>
+        public static FamilyParameter CreateTypeParam(this FamilyManager self,
+            RevitParam revitParam,
+            ForgeTypeId group,
+            Category category) {
+                throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр типа у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="parameterType">Тип создаваемого параметра у семейства.</param>
+        /// <returns>Возвращает новый созданный параметр типа у семейства.</returns>
+        public static FamilyParameter CreateTypeParam(this FamilyManager self,
+            RevitParam revitParam,
+            ForgeTypeId group,
+            ForgeTypeId parameterType) {
+                throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр экземпляра семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="category">Категория, к которой привязывается новый параметр семейства.</param>
+        /// <returns>Возвращает новый созданный параметр экземпляра у семейства.</returns>
+        public static FamilyParameter CreateInstanceParam(this FamilyManager self,
+            RevitParam revitParam,
+            ForgeTypeId group,
+            Category category) {
+                throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Создает параметр экземпляра у семейства.
+        /// </summary>
+        /// <param name="self">Менеджер семейства.</param>
+        /// <param name="revitParam">Параметр Revit.</param>
+        /// <param name="group">Группа, к которой принадлежит параметр семейства.</param>
+        /// <param name="parameterType">Тип создаваемого параметра у семейства.</param>
+        /// <returns>Возвращает новый созданный параметр экземпляра у семейства.</returns>
+        public static FamilyParameter CreateInstanceParam(this FamilyManager self,
+            RevitParam revitParam,
+            ForgeTypeId group,
+            ForgeTypeId parameterType) {
+                throw new NotImplementedException();
+        }
+
+#endif
+    }
+}
