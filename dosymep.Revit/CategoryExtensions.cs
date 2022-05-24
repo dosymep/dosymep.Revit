614c828e3d44a21281af3c196257d8547955f02c
dosymep <dosymep@gmail.com>
2022-05-24 11:53:43 +0300

    Добавил метод проверки BuiltInCategory на идентификатор категории

--- dosymep.Revit/CategoryExtensions.cs
+++ dosymep.Revit/CategoryExtensions.cs
@@ -5,6 +5,14 @@ namespace dosymep.Revit {
     /// Класс расширений категории.
     /// </summary>
     public static class CategoryExtensions {
-
+        /// <summary>
+        /// Проверяет <see cref="BuiltInCategory"/> ли является она идентификатором категории.
+        /// </summary>
+        /// <param name="self">Категория.</param>
+        /// <param name="builtInCategory">Встроенная категория.</param>
+        /// <returns>Возвращает true - если <see cref="BuiltInCategory"/> является идентификатором категории, иначе false.</returns>
+        public static bool IsId(this Category self, BuiltInCategory builtInCategory) {
+            return self == null ? false : self.Id.IntegerValue == (int) builtInCategory;
+        }
     }
 }
