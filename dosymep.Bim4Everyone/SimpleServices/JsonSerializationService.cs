e8f3bcd92a860a577f05b9982bee7bdddc6f2027
dosymep <dosymep@gmail.com>
2022-05-25 12:02:40 +0300

    Добавил сервис сериализации json

---
+++ dosymep.Bim4Everyone/SimpleServices/JsonSerializationService.cs
@@ -0,0 +1,15 @@
+﻿using dosymep.SimpleServices;
+
+using pyRevitLabs.Json;
+
+namespace dosymep.Bim4Everyone.SimpleServices {
+    internal class JsonSerializationService : ISerializationService {
+        public string Serialize<T>(T @object) {
+            return JsonConvert.SerializeObject(@object);
+        }
+
+        public T Deserialize<T>(string text) {
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+    }
+}
