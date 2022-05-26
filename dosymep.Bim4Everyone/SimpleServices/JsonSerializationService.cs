e0eebe2a06b1e40a85284309a7171b3aed07839b
dosymep <dosymep@gmail.com>
2022-05-26 17:30:52 +0300

    Добавил настройки для сериализатора

--- dosymep.Bim4Everyone/SimpleServices/JsonSerializationService.cs
+++ dosymep.Bim4Everyone/SimpleServices/JsonSerializationService.cs
@@ -4,14 +4,20 @@ using pyRevitLabs.Json;
 
 namespace dosymep.Bim4Everyone.SimpleServices {
     internal class JsonSerializationService : ISerializationService {
+        private readonly JsonSerializerSettings _settings;
+
+        public JsonSerializationService(JsonSerializerSettings settings) {
+            _settings = settings;
+        }
+        
         public string FileExtension => ".json";
 
         public string Serialize<T>(T @object) {
-            return JsonConvert.SerializeObject(@object);
+            return JsonConvert.SerializeObject(@object, _settings);
         }
 
         public T Deserialize<T>(string text) {
-            return JsonConvert.DeserializeObject<T>(text);
+            return JsonConvert.DeserializeObject<T>(text, _settings);
         }
     }
 }
