eaa779707c1d1fb6a59efc678bea7d435ef10eb3
dosymep <dosymep@gmail.com>
2022-05-26 11:56:29 +0300

    Добавил свойство расширения файла сериализации

--- dosymep.Bim4Everyone/SimpleServices/JsonSerializationService.cs
+++ dosymep.Bim4Everyone/SimpleServices/JsonSerializationService.cs
@@ -4,6 +4,8 @@ using pyRevitLabs.Json;
 
 namespace dosymep.Bim4Everyone.SimpleServices {
     internal class JsonSerializationService : ISerializationService {
+        public string FileExtension => ".json";
+
         public string Serialize<T>(T @object) {
             return JsonConvert.SerializeObject(@object);
         }
