848700cfe7798e83e334cf13877ce4cfe48dcb01
dosymep <dosymep@gmail.com>
2022-06-01 10:41:36 +0300

    Добавил сервис информации о выполняемом плагине

---
+++ dosymep.Bim4Everyone/SimpleServices/IPluginInfoService.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace dosymep.Bim4Everyone.SimpleServices {
+    /// <summary>
+    /// Интерфейс по предоставлению выполняемого плагина.
+    /// </summary>
+    public interface IPluginInfoService {
+        /// <summary>
+        /// Сборка выполняемого плагина.
+        /// </summary>
+        Assembly PluginAssembly { get; set; }
+    }
+}
---
+++ dosymep.Bim4Everyone/SimpleServices/PluginInfoService.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace dosymep.Bim4Everyone.SimpleServices {
+    /// <summary>
+    /// Класс информации о выполняемом плагине.
+    /// </summary>
+    public class PluginInfoService : IPluginInfoService {
+        /// <inheritdoc />
+        public Assembly PluginAssembly { get; set; }
+    }
+}
