6b4631e7855a9030188ed03ad7bcf6df61379151
dosymep <dosymep@gmail.com>
2022-06-07 10:30:44 +0300

    Добавил класс пользовательского параметра

---
+++ dosymep.Bim4Everyone/CustomParams/CustomParam.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+using Autodesk.Revit.DB;
+
+using dosymep.Revit;
+
+namespace dosymep.Bim4Everyone.CustomParams {
+    /// <summary>
+    /// Пользовательский параметр Revit.
+    /// </summary>
+    public class CustomParam : RevitParam {
+        /// <summary>
+        /// Создает экземпляр пользовательского параметра Revit.
+        /// </summary>
+        /// <param name="paramId">Идентификатор параметра.</param>
+        public CustomParam(string paramId)
+            : base(paramId) {
+        }
+
+        /// <inheritdoc />
+        public override bool IsExistsParam(Document document) {
+            return document.IsExistsParam(Name);
+        }
+
+        /// <inheritdoc />
+        public override (Definition Definition, Binding Binding) GetParamBinding(Document document) {
+            return document.GetParameterBindings().FirstOrDefault(item => item.Definition.Name.Equals(Name));
+        }
+
+        /// <inheritdoc />
+        public override ParameterElement GetRevitParamElement(Document document) {
+            return document.GetProjectParamElements().FirstOrDefault(item => item.Name.Equals(Name));
+        }
+
+        /// <inheritdoc />
+        public override Parameter GetParam(Element element) {
+            return element.GetParam(Name);
+        }
+    }
+}
