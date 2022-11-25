e345d1219c603b035bc3965596c775308469d5f7
dosymep <dosymep@gmail.com>
2022-11-25 09:32:48 +0300

    Добавил метод установки текста в статус баре в приложении Revit

---
+++ dosymep.Revit/UIApplicationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Autodesk.Revit.UI;
+
+using dosymep.Revit.WinApi;
+
+namespace dosymep.Revit {
+    /// <summary>
+    /// Расширение для класса <see cref="Autodesk.Revit.UI.UIApplication"/>.
+    /// </summary>
+    public static class UIApplicationExtensions {
+        /// <summary>
+        /// Устанавливает текст в статус баре в приложении Revit.
+        /// <p>https://jeremytammik.github.io/tbc/a/0534_status_bar_text.htm</p>
+        /// </summary>
+        /// <param name="uiApplication">Приложение.</param>
+        /// <param name="text">Отображаемый текст в статус баре.</param>
+        public static void SetStatusBarText(this UIApplication uiApplication, string text) {
+            IntPtr statusBar = NativeMethods.FindWindowEx(uiApplication.MainWindowHandle,
+                IntPtr.Zero, "msctls_statusbar32", "");
+
+            if(statusBar != IntPtr.Zero) {
+                NativeMethods.SetWindowText(statusBar, text);
+            }
+        }
+    }
+}
---
+++ dosymep.Revit/WinApi/NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dosymep.Revit.WinApi {
+    internal class NativeMethods {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int SetWindowText(IntPtr hWnd, 
+            string lpString);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, 
+            IntPtr hwndChildAfter, 
+            string lpszClass,
+            string lpszWindow);
+    }
+}
