dc349fe41d69aaaff85043e1c490f68a8d5d69c3
dosymep <dosymep@gmail.com>
2023-01-19 16:09:01 +0300

    Добавил расширения для классов ревита

---
+++ dosymep.Revit/RoomExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace dosymep.Revit {
+    /// <summary>
+    /// Расширения класса <see cref="Room"/>.
+    /// </summary>
+    public static class RoomExtensions {
+        /// <summary>
+        /// Опции получения границ помещений по умолчанию.
+        /// </summary>
+        public static readonly SpatialElementBoundaryOptions DefaultBoundaryOptions
+            = new SpatialElementBoundaryOptions();
+
+        /// <summary>
+        /// Проверяет является элемент избыточным.
+        /// </summary>
+        /// <param name="room">Проверяемый элемент.</param>
+        /// <param name="options">Опции получение границ элемента.</param>
+        /// <returns>Возвращает true если элемент является избыточным, иначе false.</returns>
+        public static bool IsRedundant(this Room room,
+            SpatialElementBoundaryOptions options = default) {
+            double roomArea = room.GetParamValueOrDefault<double>(BuiltInParameter.ROOM_AREA);
+            return roomArea == 0 && room.GetBoundarySegments(options ?? DefaultBoundaryOptions).Count > 0;
+        }
+
+        /// <summary>
+        /// Проверяет является элемент не избыточным.
+        /// </summary>
+        /// <param name="room">Проверяемый элемент.</param>
+        /// <param name="options">Опции получение границ элемента.</param>
+        /// <returns>Возвращает true если элемент является не избыточным, иначе false.</returns>
+        public static bool IsNotRedundant(this Room room,
+            SpatialElementBoundaryOptions options = default) {
+            return !room.IsRedundant(options);
+        }
+
+        /// <summary>
+        /// Проверяет является элемент замкнутым.
+        /// </summary>
+        /// <param name="room">Проверяемый элемент.</param>
+        /// <param name="options">Опции получение границ элемента.</param>
+        /// <returns>Возвращает true если элемент является замкнутым, иначе false.</returns>
+        public static bool IsEnclosed(this Room room,
+            SpatialElementBoundaryOptions options = default) {
+            double roomArea = room.GetParamValueOrDefault<double>(BuiltInParameter.ROOM_AREA);
+            return roomArea > 0 && room.GetBoundarySegments(options ?? DefaultBoundaryOptions).Count > 0;
+        }
+
+        /// <summary>
+        /// Проверяет является элемент не замкнутым.
+        /// </summary>
+        /// <param name="room">Проверяемый элемент.</param>
+        /// <param name="options">Опции получение границ элемента.</param>
+        /// <returns>Возвращает true если элемент является не замкнутым, иначе false.</returns>
+        public static bool IsNotEnclosed(this Room room,
+            SpatialElementBoundaryOptions options = default) {
+            return !room.IsEnclosed(options);
+        }
+    }
+}
---
+++ dosymep.Revit/SpatialElementExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace dosymep.Revit {
+    /// <summary>
+    /// Расширения класса <see cref="SpatialElement"/>.
+    /// </summary>
+    public static class SpatialElementExtensions {
+        /// <summary>
+        /// Опции получения границ размещаемых элементов по умолчанию.
+        /// </summary>
+        public static readonly SpatialElementBoundaryOptions DefaultBoundaryOptions
+            = new SpatialElementBoundaryOptions();
+
+        /// <summary>
+        /// Находит самопересечения у границ размещаемых элементов.
+        /// </summary>
+        /// <param name="spatialElement">Проверяемый элемент.</param>
+        /// <param name="options">Опции получение границ элемента.</param>
+        /// <returns>Возвращает true если были найдены самопересечения, иначе false.</returns>
+        public static bool IsSelfCrossBoundaries(this SpatialElement spatialElement,
+            SpatialElementBoundaryOptions options = default) {
+            var segments = spatialElement.GetBoundarySegments(options ?? DefaultBoundaryOptions)
+                .FirstOrDefault();
+
+            if(segments == null) {
+                return false;
+            }
+
+            var enumSegments = segments.Zip(segments.Skip(2),
+                (leftSegment, rightSegment) => (leftSegment, rightSegment));
+
+            foreach((BoundarySegment leftSegment, BoundarySegment rightSegment) in enumSegments) {
+                Curve leftCurve = leftSegment.GetCurve();
+                Curve rightCurve = rightSegment.GetCurve();
+
+                SetComparisonResult intersect = leftCurve.Intersect(rightCurve,
+                    out IntersectionResultArray _);
+
+                if(intersect == SetComparisonResult.Overlap) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
