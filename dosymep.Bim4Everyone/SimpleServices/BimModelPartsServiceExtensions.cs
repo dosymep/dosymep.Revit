b8d042407bd9cf776da9afdf5e5c35b776784b47
dosymep <dosymep@gmail.com>
2023-03-30 11:30:51 +0300

    Добавил методы для типа и экземпляра связи Revit

--- dosymep.Bim4Everyone/SimpleServices/BimModelPartsServiceExtensions.cs
+++ dosymep.Bim4Everyone/SimpleServices/BimModelPartsServiceExtensions.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 
 using Autodesk.Revit.DB;
 
-namespace dosymep.Bim4Everyone.SimpleServices
-{
+namespace dosymep.Bim4Everyone.SimpleServices {
     /// <summary>
     /// Класс расширений сервиса по получению разделов BIM модели.
     /// </summary>
@@ -97,6 +96,190 @@ namespace dosymep.Bim4Everyone.SimpleServices
             }
 
             return service.InAnyBimModelParts(document.Title, bimModelParts);
+        }
+
+        /// <summary>
+        /// Получает раздел по переданному документу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkType">Тип связи Revit.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        internal static BimModelPart GetBimModelPart(this IBimModelPartsService service, RevitLinkType revitLinkType) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkType == null) {
+                throw new ArgumentNullException(nameof(revitLinkType));
+            }
+
+            return service.GetBimModelPart(revitLinkType.Name);
+        }
+
+        /// <summary>
+        /// Проверят является ли документ переданному разделу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkType">Тип связи Revit.</param>
+        /// <param name="bimModelPart">Раздел BIM модели.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        public static bool InAnyBimModelParts(this IBimModelPartsService service, RevitLinkType revitLinkType,
+            BimModelPart bimModelPart) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkType == null) {
+                throw new ArgumentNullException(nameof(revitLinkType));
+            }
+
+            if(bimModelPart == null) {
+                throw new ArgumentNullException(nameof(bimModelPart));
+            }
+
+            return service.InAnyBimModelParts(revitLinkType.Name, bimModelPart);
+        }
+
+        /// <summary>
+        /// Проверят является ли документ переданному разделу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkType">Тип связи Revit.</param>
+        /// <param name="bimModelParts">Раздел BIM модели.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        public static bool InAnyBimModelParts(this IBimModelPartsService service, RevitLinkType revitLinkType,
+            params BimModelPart[] bimModelParts) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkType == null) {
+                throw new ArgumentNullException(nameof(revitLinkType));
+            }
+
+            if(bimModelParts == null) {
+                throw new ArgumentNullException(nameof(bimModelParts));
+            }
+
+            return service.InAnyBimModelParts(revitLinkType.Name, bimModelParts);
+        }
+
+        /// <summary>
+        /// Проверят является ли документ переданному разделу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkType">Тип связи Revit.</param>
+        /// <param name="bimModelParts">Раздел BIM модели.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        public static bool InAnyBimModelParts(this IBimModelPartsService service, RevitLinkType revitLinkType,
+            IEnumerable<BimModelPart> bimModelParts) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkType == null) {
+                throw new ArgumentNullException(nameof(revitLinkType));
+            }
+
+            if(bimModelParts == null) {
+                throw new ArgumentNullException(nameof(bimModelParts));
+            }
+
+            return service.InAnyBimModelParts(revitLinkType.Name, bimModelParts);
+        }
+
+        /// <summary>
+        /// Получает раздел по переданному документу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkInstance">Экземпляр связи Revit.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        internal static BimModelPart GetBimModelPart(this IBimModelPartsService service,
+            RevitLinkInstance revitLinkInstance) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkInstance == null) {
+                throw new ArgumentNullException(nameof(revitLinkInstance));
+            }
+
+            return service.GetBimModelPart(revitLinkInstance.Name);
+        }
+
+        /// <summary>
+        /// Проверят является ли документ переданному разделу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkInstance">Экземпляр связи Revit.</param>
+        /// <param name="bimModelPart">Раздел BIM модели.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        public static bool InAnyBimModelParts(this IBimModelPartsService service,
+            RevitLinkInstance revitLinkInstance,
+            BimModelPart bimModelPart) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkInstance == null) {
+                throw new ArgumentNullException(nameof(revitLinkInstance));
+            }
+
+            if(bimModelPart == null) {
+                throw new ArgumentNullException(nameof(bimModelPart));
+            }
+
+            return service.InAnyBimModelParts(revitLinkInstance.Name, bimModelPart);
+        }
+
+        /// <summary>
+        /// Проверят является ли документ переданному разделу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkInstance">Экземпляр связи Revit.</param>
+        /// <param name="bimModelParts">Раздел BIM модели.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        public static bool InAnyBimModelParts(this IBimModelPartsService service,
+            RevitLinkInstance revitLinkInstance,
+            params BimModelPart[] bimModelParts) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkInstance == null) {
+                throw new ArgumentNullException(nameof(revitLinkInstance));
+            }
+
+            if(bimModelParts == null) {
+                throw new ArgumentNullException(nameof(bimModelParts));
+            }
+
+            return service.InAnyBimModelParts(revitLinkInstance.Name, bimModelParts);
+        }
+
+        /// <summary>
+        /// Проверят является ли документ переданному разделу.
+        /// </summary>
+        /// <param name="service">Сервис.</param>
+        /// <param name="revitLinkInstance">"Экземпляр связи Revit.</param>
+        /// <param name="bimModelParts">Раздел BIM модели.</param>
+        /// <returns>Возвращает раздел модели по документу.</returns>
+        public static bool InAnyBimModelParts(this IBimModelPartsService service,
+            RevitLinkInstance revitLinkInstance,
+            IEnumerable<BimModelPart> bimModelParts) {
+            if(service == null) {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if(revitLinkInstance == null) {
+                throw new ArgumentNullException(nameof(revitLinkInstance));
+            }
+
+            if(bimModelParts == null) {
+                throw new ArgumentNullException(nameof(bimModelParts));
+            }
+
+            return service.InAnyBimModelParts(revitLinkInstance.Name, bimModelParts);
         }
     }
 }
