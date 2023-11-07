87f49fd1033f3dfcaa6fbe6bf99c556038226d64
dosymep <dosymep@gmail.com>
2023-11-07 14:20:15 +0300

    Обновил сборку проекта

--- build/Build.cs
+++ build/Build.cs
@@ -1,8 +1,14 @@
 using System.IO;
+using System.Linq;
+
+using dosymep.Nuke.RevitVersions;
 
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild {
     /// Support plugins are available for:
@@ -19,22 +25,41 @@ class Build : NukeBuild {
     [Parameter] readonly AbsolutePath Output = RootDirectory / "bin";
     [Parameter] readonly string DocsOutput = Path.Combine("docs", "_site");
     [Parameter] readonly string DocsConfig = Path.Combine("docs", "docfx.json");
+    
+    /// <summary>
+    /// Min Revit version.
+    /// </summary>
+    [Parameter("Min Revit version.")] readonly RevitVersion MinVersion = RevitVersion.Rv2020;
+
+    /// <summary>
+    /// Max Revit version.
+    /// </summary>
+    [Parameter("Max Revit version.")] readonly RevitVersion MaxVersion = RevitVersion.Rv2024;
 
     Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
-
-    Target Restore => _ => _
-        .Executes(() =>
-        {
+        .Executes(() => {
+            Output.CreateOrCleanDirectory();
+            (RootDirectory / DocsOutput).CreateOrCleanDirectory();
+            RootDirectory.GlobDirectories("**/bin", "**/obj")
+                .Where(item => item != (RootDirectory / "build" / "bin"))
+                .Where(item => item != (RootDirectory / "build" / "obj"))
+                .DeleteDirectories();
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
+        .DependsOn(Clean)
+        .Executes(() => {
+            DotNetBuild(s => s
+                .EnableForce()
+                .DisableNoRestore()
+                .SetConfiguration(Configuration)
+                .When(IsServerBuild, _ => _
+                    .EnableContinuousIntegrationBuild())
+                .CombineWith(RevitVersion.GetRevitVersions(MinVersion, MaxVersion), (settings, version) => {
+                    return settings
+                        .SetOutputDirectory(Output / version)
+                        .SetProperty("RevitVersion", (int) version);
+                }));
         });
 
     Target DocsCompile => _ => _
