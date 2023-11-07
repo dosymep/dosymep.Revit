c47735d4a12f47c67638c8f0d63bb8e4c3c51a9b
dosymep <dosymep@gmail.com>
2023-11-07 10:28:30 +0300

    Добавил генерацию документации

--- build/Build.cs
+++ build/Build.cs
@@ -1,28 +1,24 @@
-using System;
-using System.Linq;
-using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using System.IO;
 
-class Build : NukeBuild
-{
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+
+class Build : NukeBuild {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter] readonly AbsolutePath Output = RootDirectory / "bin";
+    [Parameter] readonly string DocsOutput = Path.Combine("docs", "_site");
+    [Parameter] readonly string DocsConfig = Path.Combine("docs", "docfx.json");
 
     Target Clean => _ => _
         .Before(Restore)
@@ -41,4 +37,27 @@ class Build : NukeBuild
         {
         });
 
+    Target DocsCompile => _ => _
+        .DependsOn(Compile)
+        .Executes(() => {
+            // В nuget.org лежит старая версия
+            ProcessTasks.StartProcess(
+                "dotnet", 
+                "tool install -g docfx");
+            
+            ProcessTasks.StartProcess(
+                "docfx",
+                DocsConfig
+                + (IsLocalBuild
+                    ? " --serve"
+                    : string.Empty),
+                RootDirectory).WaitForExit();
+
+            // DocFXBuild(s => s
+            //     .EnableForceRebuild()
+            //     .SetServe(IsLocalBuild)
+            //     .SetOutputFolder(DocsOutput)
+            //     .SetProcessWorkingDirectory(RootDirectory)
+            // );
+        });
 }
