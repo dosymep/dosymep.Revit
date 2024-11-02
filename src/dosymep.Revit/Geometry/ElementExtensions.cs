77271228587070c357589971eecc1885d5f6cfa0
vlastroG <vlastroganov@gmail.com>
2024-11-02 09:51:09 +0300

    Обновлены методы расширения ElementExtensions.GetSolids
    Исправлена ошибка NRE, когда get_Geometry возвращает null,
    добавлена перегрузка с указанием Options

--- src/dosymep.Revit/Geometry/ElementExtensions.cs
+++ src/dosymep.Revit/Geometry/ElementExtensions.cs
@@ -10,19 +10,52 @@ namespace dosymep.Revit.Geometry {
     /// </summary>
     public static class ElementExtensions {
         private static readonly Options _options = new Options() {
-            ComputeReferences = true, 
-            IncludeNonVisibleObjects = false, DetailLevel = ViewDetailLevel.Fine
+            ComputeReferences = true,
+            IncludeNonVisibleObjects = false,
+            DetailLevel = ViewDetailLevel.Fine
         };
-        
+
         /// <summary>
-        /// Возвращает все <see cref="Solid"/> объекты из <see cref="Autodesk.Revit.DB.Element"/>.
+        /// Возвращает все <see cref="Solid"/> объекты из <see cref="Autodesk.Revit.DB.Element"/> с <see cref="Autodesk.Revit.DB.Options"/> по умолчанию<br/>
+        /// в координатах документа этого элемента относительно начала проекта.<br/>
+        /// Настройки <see cref="Autodesk.Revit.DB.Options"/> по умолчанию:<br/>
+        /// <code>
+        /// ComputeReferences = true
+        /// IncludeNonVisibleObjects = false
+        /// DetailLevel = ViewDetailLevel.Fine.
+        /// </code>
         /// </summary>
         /// <param name="element">Элемент у которого берутся <see cref="Solid"/>.</param>
         /// <returns>Возвращает все <see cref="Solid"/> объекты из <see cref="Autodesk.Revit.DB.Element"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">Исключение, если один из обязательных параметров null.</exception>
         public static IEnumerable<Solid> GetSolids(this Element element) {
-            return element.get_Geometry(_options)
-                .SelectMany(item => item.GetSolids())
-                .ToList();
-        } 
+            if(element is null) {
+                throw new System.ArgumentNullException(nameof(element));
+            }
+
+            return element.GetSolids(_options);
+        }
+
+        /// <summary>
+        /// Возвращает все <see cref="Solid"/> объекты из <see cref="Autodesk.Revit.DB.Element"/> с заданными <see cref="Autodesk.Revit.DB.Options"/><br/>
+        /// в координатах документа этого элемента относительно начала проекта.
+        /// </summary>
+        /// <param name="element">Элемент у которого берутся <see cref="Solid"/>.</param>
+        /// <param name="options">Настройки получения <see cref="Solid"/> из <see cref="Autodesk.Revit.DB.Element"/></param>
+        /// <returns>Возвращает все <see cref="Solid"/> объекты из <see cref="Autodesk.Revit.DB.Element"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">Исключение, если один из обязательных параметров null.</exception>
+        public static IEnumerable<Solid> GetSolids(this Element element, Options options) {
+            if(element is null) {
+                throw new System.ArgumentNullException(nameof(element));
+            }
+
+            if(options is null) {
+                throw new System.ArgumentNullException(nameof(options));
+            }
+
+            return element.get_Geometry(options)
+                ?.SelectMany(item => item.GetSolids())
+                .ToList() ?? Enumerable.Empty<Solid>();
+        }
     }
 }
