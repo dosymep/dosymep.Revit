58db5558f3c05dd654fa287fe6932cfc95e67c89
Biseuv Olzhas <dosymep@gmail.com>
2025-02-06 17:18:52 +0300

    Обновлён способ проверки самопересечений границ помещений (#15)

--- src/dosymep.Revit/SpatialElementExtensions.cs
+++ src/dosymep.Revit/SpatialElementExtensions.cs
@@ -26,20 +26,41 @@ namespace dosymep.Revit {
         /// <returns>Возвращает true если были найдены самопересечения, иначе false.</returns>
         public static bool IsSelfCrossBoundaries(this SpatialElement spatialElement,
             SpatialElementBoundaryOptions options = default) {
-            var boundarySegments = spatialElement.GetBoundarySegments(options ?? DefaultBoundaryOptions);
-            foreach(IList<BoundarySegment> segments in boundarySegments) {
-                var enumSegments = segments.Zip(segments.Skip(2),
-                    (leftSegment, rightSegment) => (leftSegment, rightSegment));
+            IList<IList<BoundarySegment>> boundarySegments =
+                spatialElement.GetBoundarySegments(options ?? DefaultBoundaryOptions);
 
-                foreach((BoundarySegment leftSegment, BoundarySegment rightSegment) in enumSegments) {
-                    Curve leftCurve = leftSegment.GetCurve();
-                    Curve rightCurve = rightSegment.GetCurve();
+            BoundarySegment[] segments = boundarySegments
+                .SelectMany(item => item)
+                .ToArray();
 
-                    SetComparisonResult intersect = leftCurve.Intersect(rightCurve,
-                        out IntersectionResultArray _);
+            for(int indexLeft = 0; indexLeft < segments.Length; indexLeft++) {
+                Curve leftCurve = segments[indexLeft].GetCurve();
 
-                    if(intersect == SetComparisonResult.Overlap) {
-                        return true;
+                for(int indexRight = 1; indexRight < segments.Length; indexRight++) {
+                    if(indexLeft - indexRight >= 0) {
+                        // pass duplicates: 1-3 and 3-1
+                        continue;
+                    }
+
+                    // true for connected segments (they overlapped)
+                    // but needs check they can be inside each other 
+                    // others overlapped and cross
+                    bool result = indexRight - indexLeft == 1
+                                  || (indexRight - indexLeft) == (segments.Length - 1);
+                    
+                    Curve rightCurve = segments[indexRight].GetCurve();
+                    SetComparisonResult intersect = leftCurve.Intersect(rightCurve);
+                    
+                    if(result) {
+                        if(intersect == SetComparisonResult.Equal
+                           || intersect == SetComparisonResult.Subset
+                           || intersect == SetComparisonResult.Superset) {
+                            return true;
+                        }
+                    } else {
+                        if(intersect == SetComparisonResult.Overlap) {
+                            return true;
+                        }
                     }
                 }
             }
