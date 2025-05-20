c3704a2766a64cfc09767eb4a8f2f3fa3207d034
Mankaev_r <romankaev@gmail.com>
2025-05-20 15:54:36 +0300

    Пуш корректной версии

--- src/dosymep.Revit/ParamExtensions.cs
+++ src/dosymep.Revit/ParamExtensions.cs
@@ -183,6 +183,32 @@ namespace dosymep.Revit {
             return BuiltInParameter.INVALID;
         }
 
+#if REVIT2020 || REVIT2021
+        /// <summary>
+        /// Заново объявляет общий параметр в проекте, меняя его группу
+        /// </summary>
+        /// <param name="definition">Определение параметра</param>
+        /// <param name="document">Документ проекта</param>
+        /// <param name="group">Группа параметра</param>
+        public static void ReInsertToGroup(this Definition definition, Autodesk.Revit.DB.Document document, BuiltInParameterGroup group) {
+            if (definition.ParameterGroup != group) {
+                Binding binding = document.ParameterBindings.get_Item(definition);
+                if (binding is ElementBinding elementBinding) {
+                    CategorySet categories = elementBinding.Categories;
+
+                    document.ParameterBindings.Remove(definition);
+
+                    if (binding is InstanceBinding) {
+                        document.ParameterBindings.Insert(definition, new InstanceBinding(categories), group);
+                    } else if (binding is TypeBinding) {
+                        document.ParameterBindings.Insert(definition, new TypeBinding(categories), group);
+                    }
+                }
+            }
+        }
+    }
+}
+#else
         /// <summary>
         /// Заново объявляет общий параметр в проекте, меняя его группу
         /// </summary>
@@ -206,3 +232,4 @@ namespace dosymep.Revit {
         }
     }
 }
+#endif
